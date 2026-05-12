0ec6f4c58176021ea6ee5e0b3073ee8f1ab69293
Roberto Edgar Geiss <robertogeiss@novohamburgo.rs.gov.br>
2026-05-12 18:17:36 -0300

    fix(notifications): add missing migration + merge conflicting 0002 migrations

---
+++ backend/apps/notifications/migrations/0003_merge_0002_notifications.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notifications', '0002_alter_notification_type'),
+        ('notifications', '0002_notification_is_archived'),
+    ]
+
+    operations = [
+    ]
